d0ddadc2ffcae6aa6f37ec4ddca1343d302cd927
KurizuLaw <CGehder@gmail.com>
2021-11-10 18:21:51 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@
 BOT_PREFIX = ("d!")
 # Bot Token
 TOKEN = ""
-APPLICATION_ID = 813474746921648168
-OWNERS = [248849392965124097]
-ROLE_CHANNELS = [775429000712421376, 813507687055753277]
+APPLICATION_ID = 
+OWNERS = []
+ROLE_CHANNELS = []
 BLACKLIST = []
 # Default cogs that I have created for the template
 STARTUP_COGS = ["cogs.help", "cogs.klaue"]          
